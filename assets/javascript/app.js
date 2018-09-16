deb23a3b4a0ff894858fbc3cd92a0273adb2fa29
SenorEdbot <eddiefranco626@gmail.com>
2018-09-15 19:46:43 -0500

    Completed Javascript. Style still needed.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,5 @@
-/* Logic Needed: 
-1. User presses a start button to start the game and start the first timer.
-2. Question Logic
-    a) If the timer runs out of time the question is considered 'unanswered' and the answer is shown for a few seconds (5)
-    b) If the user guesses the correct answer the question is considered 'correct' and the answer is shown for a few seconds (5)
-    c) If the user guesses incorrectly the question is considered 'incorrect' and and "Oh, No! The correct answer was" and then show the correct answer screen (5)
-3. After each answered screen the game should automatically display the next question and start the timer at 30 seconds.
-4. After the game is completed a summary screen is shown that displays the number of 'unanswered', 'correct', or 'incorrect' answers and displays a "Start Over?" button. 
-5. The start over button does not reload the page and starts the game over at the beginning. 
-*/
-
-/**
- * When an answer button is pressed
- *  Check to see what position the question array is on
- *  Determine if the correct answer was associated to that button for that question in the array
- */
+//Waiting for the document to load
+$(document).ready(function(){
 
 //Setting up the Questions Array
 var questions = [
@@ -89,10 +75,12 @@ var correctAnswerArr = [
     "9",
     "Drake"
 ];
-//Setting the 4 required variables
-var correct, incorrect, unanswered, questionCounter = 0;
-var questionsRemaining = true;
-var correctAnswer = false;
+//Setting the 5 required variables
+var correct = 0; 
+var incorrect = 0;
+var unanswered = 0;
+var questionCounter = 0;
+var infoText;
 
 //Setting the jQuery ID's to variables
 var timerDiv = $('#timerDiv');
@@ -105,6 +93,7 @@ var ans4Button = $('#answer4');
 var startButton = $('#startButton');
 var resetButton = $('#resetButton');
 
+//setting up the timer object
 var cdClockID;
 var clockRunning = false;
 var countdownClock = {
@@ -129,19 +118,12 @@ var countdownClock = {
         timerDiv.text(converted);
         if(countdownClock.time === 0) {
             unanswered += 1;
+            infoText = "Out of Time!";
             countdownClock.stop();
             countdownClock.reset();
             correctAnswer = false;
-            //display the correct answer function (do a timeout of 7 seconds)
-            //setInterval(dispCorrectAns, 7000);
-            questionCounter += 1;
-            if(questionCounter < 9) {
-                gameLogic.questionSetup();
-                countdownClock.start();
-            } else {
-                //Run the logic for the end of the game.
-            }
-
+            gameLogic.dispCorrectAns();
+            setTimeout(gameLogic.questionIncrement,2000);
         }
     },
     timeConverter: function(t) {
@@ -152,44 +134,55 @@ var countdownClock = {
         return seconds;
     }
 };
+
+//setting up the Game Logic Functions
 var gameLogic = {
     dispCorrectAns: function() {
         $('.answers').hide();
         correctAnsDiv.show();
         switch(questionCounter){
             case 0:
-                questionDiv.text();
+                questionDiv.text(infoText);
                 //Display the image.
-                correctAnsDiv.text("The correct answer was :" + correctAnswerArr[0])
+                correctAnsDiv.text("The correct answer was: " + correctAnswerArr[0])
                 break;
             case 1:
-                questionDiv.text("The correct answer was :" + correctAnswerArr[1]);
+                questionDiv.text(infoText);
+                correctAnsDiv.text("The correct answer was: " + correctAnswerArr[1]);
                 break;
             case 2:
-                questionDiv.text("The correct answer was :" + correctAnswerArr[2]);
+                questionDiv.text(infoText);
+                correctAnsDiv.text("The correct answer was: " + correctAnswerArr[2]);
                 break;
             case 3:
-                questionDiv.text("The correct answer was :" + correctAnswerArr[3]);
+                questionDiv.text(infoText);
+                correctAnsDiv.text("The correct answer was: " + correctAnswerArr[3]);
                 break;
             case 4:
-                questionDiv.text("The correct answer was :" + correctAnswerArr[4]);
+                questionDiv.text(infoText);
+                correctAnsDiv.text("The correct answer was: " + correctAnswerArr[4]);
                 break;
             case 5:
-                questionDiv.text("The correct answer was :" + correctAnswerArr[5]);
+                questionDiv.text(infoText);
+                correctAnsDiv.text("The correct answer was: " + correctAnswerArr[5]);
                 break;
             case 6:
-                questionDiv.text("The correct answer was :" + correctAnswerArr[6]);
+                questionDiv.text(infoText);
+                correctAnsDiv.text("The correct answer was: " + correctAnswerArr[6]);
                 break;
             case 7:
-                questionDiv.text("The correct answer was :" + correctAnswerArr[7]);
+                questionDiv.text(infoText);
+                correctAnsDiv.text("The correct answer was: " + correctAnswerArr[7]);
                 break;
             case 8:
-                questionDiv.text("The correct answer was :" + correctAnswerArr[8]);
+                questionDiv.text(infoText);
+                correctAnsDiv.text("The correct answer was: " + correctAnswerArr[8]);
                 break;
             case 9:
-                questionDiv.text("The correct answer was :" + correctAnswerArr[9]);
+                questionDiv.text(infoText);
+                correctAnsDiv.text("The correct answer was: " + correctAnswerArr[9]);
                 break;
-        }
+        };
     },
     questionSetup: function() {
         correctAnsDiv.hide();
@@ -199,54 +192,88 @@ var gameLogic = {
         ans2Button.text(answerArr2[questionCounter]);
         ans3Button.text(answerArr3[questionCounter]);
         ans4Button.text(answerArr4[questionCounter]);
-    }
-}
-
-var answerChecker = function(x) {
-    if(x === correctAnswerArr[questionCounter]) {
-        correct += 1;
-        countdownClock.stop();
-        countdownClock.reset();
-        correctAnswer = true;
-        //timeout for displaying the correct answer screen here
+    },
+    answerChecker : function(x) {
+        if(x === correctAnswerArr[questionCounter]) {
+            correct += 1;
+            infoText = "Correct!"
+            countdownClock.stop();
+            countdownClock.reset();
+            correctAnswer = true;
+            gameLogic.dispCorrectAns();
+            setTimeout(gameLogic.questionIncrement,2000);
+        } else {
+            incorrect += 1;
+            infoText = "Uh Oh! Incorrect."
+            countdownClock.stop();
+            countdownClock.reset();
+            correctAnswer = false;
+            gameLogic.dispCorrectAns();
+            setTimeout(gameLogic.questionIncrement,2000);
+        }
+    },
+    questionIncrement: function(){
         questionCounter += 1;
-            if(questionCounter < 9) {
+            if(questionCounter < 10) {
                 gameLogic.questionSetup();
                 countdownClock.start();
             } else {
                 //Run the logic for the end of the game.
-            }
-    } else {
-        incorrect += 1;
-        countdownClock.stop();
-        countdownClock.reset();
-        correctAnswer = false;
-        //timeout for displaying the correct answer
-        questionCounter += 1;
-            if(questionCounter < 9) {
-                gameLogic.questionSetup();
-                countdownClock.start();
-            } else {
-                //Run the logic for the end of the game.
+                $('.answers').hide();
+                timerDiv.hide();
+                questionDiv.hide();
+                correctAnsDiv.show().html("<p>Correct: " + correct + "<br />Incorrect: "+incorrect+"<br />Unanswered: "+unanswered);
+                resetButton.show();
             }
     }
-}
-
-
-
-//Waiting for the document to load
-$(document).ready(function(){
+};
 
     //What happens when the start button is clicked
-    startButton.on('click',function(event){
+    startButton.on('click',function(){
 
         //Hides the start button after pressing it. 
         startButton.hide();
         //Set first question and start the timer?
         $('.answers').show();
-        questionSetup();
+        gameLogic.questionSetup();
         countdownClock.start();
 
     });
+
+    ans1Button.on('click', function(){
+        event.preventDefault();
+        var userGuess1 = answerArr1[questionCounter];
+        gameLogic.answerChecker(userGuess1);
+    });
+    ans2Button.on('click', function(){
+        event.preventDefault();
+        var userGuess2 = answerArr2[questionCounter];
+        gameLogic.answerChecker(userGuess2);
+    });
+    ans3Button.on('click', function(){
+        event.preventDefault();
+        var userGuess3 = answerArr3[questionCounter];
+        gameLogic.answerChecker(userGuess3);
+    });
+    ans4Button.on('click', function(){
+        event.preventDefault();
+        var userGuess4 = answerArr4[questionCounter];
+        gameLogic.answerChecker(userGuess4);
+    });
+
+    resetButton.on('click', function(){
+        correct = 0;
+        incorrect = 0;
+        unanswered = 0;
+        questionCounter = 0;
+        infoText = '';
+        $('.answers').show();
+        questionDiv.empty().show();
+        correctAnsDiv.empty().hide();
+        timerDiv.show();
+        resetButton.hide();
+        gameLogic.questionSetup();
+        countdownClock.start();
+    })
 
 });
